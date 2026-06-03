d86f6dec2ac8bf14280ac915f1df84bb41fa46ac
Bhavishay Goyal 1004 <bhavishagoyal8459@gmail.com>
2026-06-03 16:32:40 +0530

    Add expense function

--- tracker.py
+++ tracker.py
@@ -1,3 +1,13 @@
+def add_expense():
+    date=input("Enter Date (DD/MM/YYYY): ")
+    amt=float(input("Enter Amount: "))
+    category=input("Enter Category (Food/Transport/Entertainment/Other): ")
+    note=input("Enter a note (otherwise NA):")
+    file = open("expense.txt","a")
+    file.write(f"{date},{amt},{category},{note}")
+    file.close()
+    
+    
 while True:
     print("========Welcome to Expense Tracker!========")
     print("1. Add an expense.")
